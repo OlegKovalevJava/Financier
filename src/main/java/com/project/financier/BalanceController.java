0d707ff361e52fc2f853628215d9cde58cae0e06
Oleg <kovalyovop@yandex.ru>
2023-01-01 18:53:02 +0300

    01/01/2023

---
+++ src/main/java/com/project/financier/BalanceController.java
@@ -0,0 +1,32 @@
+package com.project.financier;
+
+import com.project.financier.model.TransferBalance;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController("/balance")
+@AllArgsConstructor
+public class BalanceController {
+
+    private final BankService bankService;
+
+    @GetMapping("/{accountId}")
+    public BigDecimal getBalance(@PathVariable Long accountId) {
+        return bankService.getBalance(accountId);
+    }
+
+    @PostMapping("/add")
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance) {
+        return bankService.addMoney(transferBalance.getTo(), transferBalance.getAmount());
+    }
+
+    @PostMapping("/transfer")
+    public void transfer(@RequestBody TransferBalance transferBalance) {
+        bankService.makeTransfer(transferBalance);
+
+    }
+
+
+}
